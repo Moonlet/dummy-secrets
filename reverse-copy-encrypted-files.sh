60234d5ab1bd4a3e8364e11a29b22c65daa0be1b
Rosca Alexandru <alex.rosca1996@gmail.com>
2025-04-04 17:51:36 +0300

    add reverse copy script, prepare for cert generation

---
+++ reverse-copy-encrypted-files.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Check if the required parameters are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <FROM_DIRECTORY> <TO_DIRECTORY>"
+    echo "Example: $0 ./target/ lb/devops"
+    exit 1
+fi
+
+FROM_DIRECTORY=$1
+TO_DIRECTORY=./roles/moonlet-secrets/files/vaults/$2
+
+# Ensure the source folder exists
+if [ ! -d "$FROM_DIRECTORY" ]; then
+    echo "Error: Source folder '$FROM_DIRECTORY' does not exist."
+    exit 1
+fi
+
+# The VAULT_ID is the same as to_directory from the Ansible script
+VAULT_ID=$(echo "$2" | sed 's/^\/\(.*\)\/$/\1/' | sed 's/^\/\(.*\)$/\1/' | sed 's/^\(.*\)\/$/\1/')
+
+# Create the destination folder if it doesn't exist
+mkdir -p "$TO_DIRECTORY"
+
+# Encrypt and copy each file from the source folder to the destination folder
+for file in $(find "$FROM_DIRECTORY" -type f); do
+    # Get the relative path of the file
+    relative_path=${file#$FROM_DIRECTORY/}
+    
+    # Create the corresponding directory structure in the destination folder
+    mkdir -p "$TO_DIRECTORY/$(dirname "$relative_path")"
+    
+    # Check if the file already exists in the destination folder
+    if [ -f "$TO_DIRECTORY/$relative_path" ]; then
+        echo "File '$TO_DIRECTORY/$relative_path' already exists. Deleting it..."
+        rm -f "$TO_DIRECTORY/$relative_path"
+    fi
+    
+    # Encrypt the file and copy it to the destination folder
+    ./secret-create.sh ${VAULT_ID} "$file"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to encrypt and copy '$file'."
+        exit 1
+    fi
+
+        # Copy the encrypted file to the destination folder
+    cp "$file" "$TO_DIRECTORY/$relative_path"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to copy '$file' to '$TO_DIRECTORY/$relative_path'."
+        exit 1
+    fi
+done
+
+echo "All files have been encrypted and copied to '$TO_DIRECTORY'."#!/bin/bash
